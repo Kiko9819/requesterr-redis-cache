ef385d2597af58b5761838e448ce71a6aad7115f
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-28 14:08:29 +0300

    Add jwt module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
