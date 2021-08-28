200eb12f326a3da4b96b8d6c763859c5d25ba6ec
Kristiyan Krastev <k.krastev.1998@gmail.com>
2021-08-28 14:08:22 +0300

    Remove method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,5 @@
-import { Controller, Get, Logger } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
-import { AppService } from './app.service';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  private logger = new Logger('Main');
-  constructor(private readonly appService: AppService) {}
-
-  @MessagePattern('say-hi')
-  getHello(): string {
-    this.logger.log('Returning hello');
-    return this.appService.getHello();
-  }
 }
